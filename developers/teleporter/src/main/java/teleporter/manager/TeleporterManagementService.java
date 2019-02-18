23faa219f1cfc24f0e367272ef86a3a098cbe63e
Benjamin DePew <bendepew@Bens-LappyTop.local>
2019-02-17 19:29:24 -0600

    city loop detection tests started

--- developers/teleporter/src/main/java/teleporter/manager/TeleporterManagementService.java
+++ developers/teleporter/src/main/java/teleporter/manager/TeleporterManagementService.java
@@ -66,7 +66,7 @@ public class TeleporterManagementService implements TeleporterManager {
 
     @Override
     public boolean loopPoosible(String startingCity) {
-        return false;
+        return true;
     }
 }
 
--- developers/teleporter/src/test/java/teleporter/manager/TeleportManagementServiceLoopTest.java
+++ developers/teleporter/src/test/java/teleporter/manager/TeleportManagementServiceLoopTest.java
@@ -1,4 +1,20 @@
 package teleporter.manager;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TeleportManagementServiceLoopTest {
+
+    @Test
+    public void givenTightLoopReturnTrue(){
+        TeleporterManager teleporterManager = new TeleporterManagementService();
+        teleporterManager.addRoute("Kiev", "Moscow");
+        teleporterManager.addRoute("Warsaw", "Moscow");
+        teleporterManager.addRoute("Warsaw", "Kiev");
+
+        boolean loopExists = teleporterManager.loopPoosible("Kiev");
+
+        Assert.assertTrue(loopExists);
+    }
+
 }
