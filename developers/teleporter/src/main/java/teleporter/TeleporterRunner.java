ade7fc90c8bb780aa5e1654c1f419bf4988f5878
Benjamin DePew <bendepew@Bens-LappyTop.local>
2019-02-17 21:14:01 -0600

    initial runner code

--- developers/teleporter/src/main/java/teleporter/TeleporterRunner.java
+++ developers/teleporter/src/main/java/teleporter/TeleporterRunner.java
@@ -8,7 +8,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import teleporter.manager.TeleporterManager;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintStream;
 import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SpringBootApplication
 public class TeleporterRunner implements CommandLineRunner{
@@ -22,7 +28,43 @@ public class TeleporterRunner implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-           //Parse input file line by line
+        Scanner scanner = new Scanner(new File("input/example.txt"));
+        PrintStream output = new PrintStream(new File("output/example.txt"));
+
+        Pattern jumpPattern = Pattern.compile("cities from (.*?) in (\\d+) jumps");
+        Pattern reachablePattern = Pattern.compile("can I teleport from (.*?) to (.*)");
+        Pattern loopPattern = Pattern.compile("loop possible from (.*)");
+
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+
+            if(line.contains("-")){
+                String[] cities = line.split("-");
+                teleporterManager.addRoute(cities[0].trim(), cities[1].trim());
+            } else if (jumpPattern.matcher(line.trim()).matches()){
+                Matcher matcher = jumpPattern.matcher(line.trim());
+                if(matcher.find()){
+                    String city = matcher.group(1);
+                    int numOfJumps = Integer.parseInt(matcher.group(2));
+                    output.println(line.trim()+": "+teleporterManager.citiesInRange(city, numOfJumps).toString().replaceAll("[\\[\\]]",""));
+                }
+            } else if (reachablePattern.matcher(line.trim()).matches()){
+                Matcher matcher = reachablePattern.matcher(line.trim());
+                if(matcher.find()){
+                    String startingCity = matcher.group(1);
+                    String destinationCity = matcher.group(2);
+                    output.println(line.trim() + ": " + (teleporterManager.cityReachable(startingCity, destinationCity) ? "yes" : "no"));
+                }
+            } else if (loopPattern.matcher(line.trim()).matches()){
+                Matcher matcher = loopPattern.matcher(line.trim());
+                if(matcher.find()){
+                    String city = matcher.group(1);
+                    output.println(line.trim() + ": " + (teleporterManager.loopPoosible(city) ? "yes" : "no"));
+
+                }
+            }
+        }
+
 
                //Add to the teleporter
 
